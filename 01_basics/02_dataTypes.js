f49c1a18a10f8b1c0dde155fa4b2d6d6d91ae0fa
Himanshi <117020662+himanshi214@users.noreply.github.com>
2024-03-16 17:25:47 +0000

    01_basics

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(Hello) // we are using nodejs,not browser
+
+//console.log(3+3) 
+
+//console.log("hddh")
+
+let name = "Himanshi"
+let age = 12
+let isLoggedIn = false
+let state;
+
+
+// number =>2 to power 53
+// bigint
+//string => ""
+// boolean => true/false
+// null => standslone value
+// undefined
+// symbol => unique 
+
+
+
+// object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,29 @@
+let score = "33abs" 
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+/*"33" => 33
+"33abs" => NaN
+true => 1; flase =>0*/
+
+let isLoggedIn = "himanshi"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+/*
+1 => true; 0 => false
+" " => false
+"himanshi" => true
+
+*/
+let someNumber = 34
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
